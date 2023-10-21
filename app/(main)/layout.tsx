235b3d944d517471b5d692f6edbfe1c5476b9b19
sasha <sasha.voloshyn359@gmail.com>
2023-10-21 17:49:48 +0300

    add prettier config

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,34 +1,32 @@
-"use client";
+'use client';
 
-import {useConvexAuth} from "convex/react";
-import {redirect} from "next/navigation";
+import { useConvexAuth } from 'convex/react';
+import { redirect } from 'next/navigation';
 
-import { Spinner } from "@/components/spinner";
+import { Spinner } from '@/components/spinner';
 
-import { Navigation } from "./_components/navigation";
+import { Navigation } from './_components/navigation';
 
-const MainLayout = ({children}:{ children: React.ReactNode;}) => {
-    const {isAuthenticated,isLoading} = useConvexAuth();
+const MainLayout = ({ children }: { children: React.ReactNode }) => {
+	const { isAuthenticated, isLoading } = useConvexAuth();
 
-    if (isLoading) {
-        return (
-            <div className='h-full flex items-center justify-center'>
-                <Spinner size='lg'/>
-            </div>
-        )
-    }
-    if (!isAuthenticated) {
-        return  redirect('/')
-    }
+	if (isLoading) {
+		return (
+			<div className='h-full flex items-center justify-center'>
+				<Spinner size='lg' />
+			</div>
+		);
+	}
+	if (!isAuthenticated) {
+		return redirect('/');
+	}
 
-    return (
-        <div className='h-full flex dark:bg-[#1F1F1F]'>
-            <Navigation/>
-            <main className='flex-1 h-full overflow-y-auto'>
-                {children}
-            </main>
-        </div>
-    );
+	return (
+		<div className='h-full flex dark:bg-[#1F1F1F]'>
+			<Navigation />
+			<main className='flex-1 h-full overflow-y-auto'>{children}</main>
+		</div>
+	);
 };
 
 export default MainLayout;
--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -1,49 +1,48 @@
-"use client";
+'use client';
 
-import {ArrowRight} from "lucide-react";
-import Link from "next/link";
+import { ArrowRight } from 'lucide-react';
+import Link from 'next/link';
 
-import {Button} from "@/components/ui/button";
-import {useConvexAuth} from "convex/react";
-import {Spinner} from "@/components/spinner";
-import {SignInButton} from "@clerk/clerk-react";
+import { Button } from '@/components/ui/button';
+import { useConvexAuth } from 'convex/react';
+import { Spinner } from '@/components/spinner';
+import { SignInButton } from '@clerk/clerk-react';
 
 const Heading = () => {
-    const {isAuthenticated, isLoading} = useConvexAuth();
+	const { isAuthenticated, isLoading } = useConvexAuth();
 
-    return (
-        <div className='max-w-3xl space-y-4'>
-            <h1 className='text-3xl sm:text-5xl md:text-6xl font-medium'>
-                Your Ideas, Documents, & Plans. Unified. Welcome to <span className='underline'>My Note</span>
-            </h1>
-            <h3 className='text-base sm:text-xl md:text-2xl font-light'>
-                <span className='font-bold underline'>My Note</span> is the connected workspace where <br/>
-                better, faster work happens.
-            </h3>
-            {isLoading && (
-                <div className='w-full flex items-center justify-center'>
-                    <Spinner size='lg'/>
-                </div>
-            )}
-            {isAuthenticated && !isLoading && (
-                <Button asChild>
-                    <Link href='/documents'>
-                        Enter My Note
-                        <ArrowRight className='h-4 w-4 ml-2'/>
-                    </Link>
-                </Button>
-            )}
-            {!isAuthenticated && !isLoading && (
-                <SignInButton mode='modal'>
-                    <Button>
-                        Get My Note free
-                        <ArrowRight className='h-4 w-4 ml-2'/>
-                    </Button>
-
-                </SignInButton>
-            )}
-        </div>
-    );
+	return (
+		<div className='max-w-3xl space-y-4'>
+			<h1 className='text-3xl sm:text-5xl md:text-6xl font-medium'>
+				Your Ideas, Documents, & Plans. Unified. Welcome to <span className='underline'>My Note</span>
+			</h1>
+			<h3 className='text-base sm:text-xl md:text-2xl font-light'>
+				<span className='font-bold underline'>My Note</span> is the connected workspace where <br />
+				better, faster work happens.
+			</h3>
+			{isLoading && (
+				<div className='w-full flex items-center justify-center'>
+					<Spinner size='lg' />
+				</div>
+			)}
+			{isAuthenticated && !isLoading && (
+				<Button asChild>
+					<Link href='/documents'>
+						Enter My Note
+						<ArrowRight className='h-4 w-4 ml-2' />
+					</Link>
+				</Button>
+			)}
+			{!isAuthenticated && !isLoading && (
+				<SignInButton mode='modal'>
+					<Button>
+						Get My Note free
+						<ArrowRight className='h-4 w-4 ml-2' />
+					</Button>
+				</SignInButton>
+			)}
+		</div>
+	);
 };
 
 export default Heading;
